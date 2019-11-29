9e8410bd9cb951d958abedb3370cf7006f5b2a77
Raphael Rochlitz <raphael.rochlitz@leibniz-liag.de>
2019-11-29 16:46:12 +0100

    custEM scripts for reproducing custEM results added in "custEM_layered_blocky/" directory custEM results for layered_earth and block model added in "data/custEM_results/" directory

---
+++ custEM_layered_blocky/run_blocky_model.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+@author:  Rochlitz.R
+"""
+
+# ########################################################################### #
+# # # # #                        layered earth                        # # # # #
+# ########################################################################### #
+# # # # #                     computation script                      # # # # #
+# ########################################################################### #
+
+from custEM.core import MOD
+import numpy as np
+
+# ###################      define physical parameters       ################# #
+
+res_ground = [0.3, 1., 2., 1000.]
+res_blocks = [10., 100., 500.]
+
+sigma_ground = np.repeat(1./np.array(res_ground), 3).reshape(-1, 3)
+sigma_ground[2, 2] *= (1./2.)  # Apply VTI anisotropy for third layer
+sigma_blocks = 1./np.array(res_blocks)
+
+frequency = 1.
+source_current = 800.
+
+# ####################### run p1 and p2 computations ######################## #
+
+for p in range(1, 3):
+
+    mod = 'p' + str(p)
+    mesh = 'blocky_model_p' + str(p)
+
+    # Initialize model instance
+    M = MOD(mod, mesh, 'E_t', p=p, overwrite=True,
+            m_dir='./meshes', r_dir='./results')
+
+    # update physical parameters
+    M.MP.update_model_parameters(f=frequency,
+                                 sigma_anom=sigma_blocks,
+                                 sigma_ground=sigma_ground,
+                                 J=source_current)
+
+    # update fem parameters and define transmitter
+    M.FE.build_var_form(s_type='line',
+                        start=[-100., 0., -550.],
+                        stop=[100., 0., -550.])
+
+    # Call solver, autoamtically convert to H-fields, and export results
+    M.solve_main_problem()
+
+    # load existing model only for interpolation purposes
+    M = MOD(mod, mesh, 'E_t', p=p, overwrite=False, load_existing=True,
+            m_dir='./meshes', r_dir='./results')
+
+    # create regular inteprolation lines in x-direction at sea floor
+    M.IB.create_line_meshes('x',  x0=-1e4, x1=1e4, y=-3e3, z=-601., n_segs=100,
+                            line_name='l1')
+    M.IB.create_line_meshes('x',  x0=-1e4, x1=1e4, y=0., z=-601., n_segs=100,
+                            line_name='l2')
+    M.IB.create_line_meshes('x',  x0=-1e4, x1=1e4, y=3e3, z=-601., n_segs=100,
+                            line_name='l3')
+
+    for line in ['l1_line_x', 'l2_line_x', 'l3_line_x']:
+        M.IB.interpolate('E_t', line)
+        M.IB.interpolate('H_t', line)
+    M.IB.synchronize()  # synchronize all processes after interpolation
