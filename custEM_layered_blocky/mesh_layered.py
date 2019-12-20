4b34fae1945f72a1000a86d083d22a1343e1e1f1
Raphael Rochlitz <raphael.rochlitz@leibniz-liag.de>
2019-12-20 18:40:05 +0100

    new 1d results

--- custEM_layered_blocky/mesh_layered.py
+++ custEM_layered_blocky/mesh_layered.py
@@ -13,6 +13,7 @@ from custEM.meshgen import meshgen_utils as mu
 from custEM.meshgen.meshgen_tools import BlankWorld
 import numpy as np
 
+
 # ######################### mesh for p1 computations ######################## #
 
 # Define refinement-paths around receiver lcoations
@@ -24,9 +25,9 @@ rx = mu.refine_rx(points, 1., 30.)
 
 # create world
 M = BlankWorld(name='layered_earth_p1', m_dir='./meshes',
-               x_dim=[-2e4, 2e4],
-               y_dim=[-2e4, 2e4],
-               z_dim=[-1e4, 1e4],
+               x_dim=[-3e4, 3e4],
+               y_dim=[-3e4, 3e4],
+               z_dim=[-5e4, 5e4],
                preserve_edges=True)
 
 # include water surface
@@ -37,10 +38,12 @@ M.build_layered_earth_mesh(4, [-600., -850., -3150.],
                            insert_struct=rx)
 
 # add transmitter in water layer at -550 m depth
-M.add_paths([mu.line_x(-100., 100., n_segs=50., z=-550.)])
+M.add_tx(mu.line_x(-100., 100., n_segs=10, z=-550.))
+
+M.there_is_always_a_bigger_world(10., 10., 10.)
 
 # call TetGen
-M.call_tetgen(tet_param='-pq1.3aA', export_vtk=True)
+M.call_tetgen(tet_param='-pq1.2aA', export_vtk=True)
 
 
 # ######################### mesh for p2 computations ######################## #
@@ -50,13 +53,13 @@ points = np.concatenate((
     mu.line_x(-1e4, 1e4, n_segs=100, y=-3e3),
     mu.line_x(-1e4, 1e4, n_segs=100),
     mu.line_x(-1e4, 1e4, n_segs=100, y=3e3)))
-rx = mu.refine_rx(points, 20., 30.)
+rx = mu.refine_rx(points, 25., 30.)
 
 # create world
 M = BlankWorld(name='layered_earth_p2', m_dir='./meshes',
                x_dim=[-1.5e4, 1.5e4],
-               y_dim=[-1e4, 1e4],
-               z_dim=[-5e3, 5e3],
+               y_dim=[-1.4e4, 1.4e4],
+               z_dim=[-5e4, 5e4],
                preserve_edges=True)
 
 # include water surface
@@ -67,7 +70,103 @@ M.build_layered_earth_mesh(4, [-600., -850., -3150.],
                            insert_struct=rx)
 
 # add transmitter in water layer at -550 m depth
-M.add_paths([mu.line_x(-100., 100., n_segs=20., z=-550.)])
+M.add_tx(mu.line_x(-100., 100., n_segs=3, z=-550.))
+
+# append boudary mesh
+M.there_is_always_a_bigger_world(10., 10., 10.)
 
 # call TetGen
 M.call_tetgen(tet_param='-pq1.6aA', export_vtk=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # # # Initial Versions # # # #
+# # # # Initial Versions # # # #
+# # # # Initial Versions # # # #
+# # # # Initial Versions # # # #
+# # # # Initial Versions # # # #
+
+
+
+
+
+
+
+
+
+## ######################### mesh for p1 computations ######################## #
+#
+## Define refinement-paths around receiver lcoations
+#points = np.concatenate((
+#    mu.line_x(-1e4, 1e4, n_segs=100, y=-3e3),
+#    mu.line_x(-1e4, 1e4, n_segs=100),
+#    mu.line_x(-1e4, 1e4, n_segs=100, y=3e3)))
+#rx = mu.refine_rx(points, 1., 30.)
+#
+## create world
+#M = BlankWorld(name='layered_earth_p1', m_dir='./meshes',
+#               x_dim=[-2e4, 2e4],
+#               y_dim=[-2e4, 2e4],
+#               z_dim=[-1e4, 1e4],
+#               preserve_edges=True)
+#
+## include water surface
+#M.build_surface()
+#
+## build halfspace mesh and extend 2D surface mesh to 3D world
+#M.build_layered_earth_mesh(4, [-600., -850., -3150.],
+#                           insert_struct=rx)
+#
+## add transmitter in water layer at -550 m depth
+#M.add_paths([mu.line_x(-100., 100., n_segs=50., z=-550.)])
+#
+## call TetGen
+#M.call_tetgen(tet_param='-pq1.3aA', export_vtk=True)
+#
+#
+## ######################### mesh for p2 computations ######################## #
+#
+## Define refinement-paths around receiver lcoations
+#points = np.concatenate((
+#    mu.line_x(-1e4, 1e4, n_segs=100, y=-3e3),
+#    mu.line_x(-1e4, 1e4, n_segs=100),
+#    mu.line_x(-1e4, 1e4, n_segs=100, y=3e3)))
+#rx = mu.refine_rx(points, 20., 30.)
+#
+## create world
+#M = BlankWorld(name='layered_earth_p2', m_dir='./meshes',
+#               x_dim=[-1.5e4, 1.5e4],
+#               y_dim=[-1e4, 1e4],
+#               z_dim=[-5e3, 5e3],
+#               preserve_edges=True)
+#
+## include water surface
+#M.build_surface()
+#
+## build halfspace mesh and extend 2D surface mesh to 3D world
+#M.build_layered_earth_mesh(4, [-600., -850., -3150.],
+#                           insert_struct=rx)
+#
+## add transmitter in water layer at -550 m depth
+#M.add_paths([mu.line_x(-100., 100., n_segs=20., z=-550.)])
+#
+## call TetGen
+#M.call_tetgen(tet_param='-pq1.6aA', export_vtk=True)
